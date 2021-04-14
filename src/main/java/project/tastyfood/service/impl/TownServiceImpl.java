d9b5e815c2e72394d53815e83c2b2b56ab2da265
monikaa98 <82385604+monikaa98@users.noreply.github.com>
2021-04-14 11:56:57 +0300

    Update TownServiceImpl.java

--- src/main/java/project/tastyfood/service/impl/TownServiceImpl.java
+++ src/main/java/project/tastyfood/service/impl/TownServiceImpl.java
@@ -30,7 +30,6 @@ public class TownServiceImpl implements TownService {
         this.townRepository = townRepository;
         this.modelMapper = modelMapper;
         this.pictureService = pictureService;
-
     }
 
     @Override
