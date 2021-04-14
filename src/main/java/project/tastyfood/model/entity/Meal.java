bac674d59f7f49aca780dc47f3dea8cc19179294
monikaa98 <82385604+monikaa98@users.noreply.github.com>
2021-04-14 12:43:46 +0300

    Update Meal.java

--- src/main/java/project/tastyfood/model/entity/Meal.java
+++ src/main/java/project/tastyfood/model/entity/Meal.java
@@ -8,7 +8,6 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-
 @Entity
 @Table(name="meals")
 @Transactional
