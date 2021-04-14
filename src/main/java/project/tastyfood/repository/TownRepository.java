79fedd4f2bcb5213a6a359e4b5bb28f68cfcd988
monikaa98 <82385604+monikaa98@users.noreply.github.com>
2021-04-14 12:54:00 +0300

    Update TownRepository.java

--- src/main/java/project/tastyfood/repository/TownRepository.java
+++ src/main/java/project/tastyfood/repository/TownRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.tastyfood.model.entity.Town;
 
-
 import java.util.Optional;
 
 @Repository
