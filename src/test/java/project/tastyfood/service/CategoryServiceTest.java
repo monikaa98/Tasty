0c46d9abc5e3c3e51f2b6a4f66d41347ef93c282
monikaa98 <82385604+monikaa98@users.noreply.github.com>
2021-04-14 12:22:39 +0300

    Update CategoryServiceTest.java

--- src/test/java/project/tastyfood/service/CategoryServiceTest.java
+++ src/test/java/project/tastyfood/service/CategoryServiceTest.java
@@ -19,7 +19,6 @@ import static org.mockito.Mockito.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 public class CategoryServiceTest {
-
     @Autowired
     private CategoryService categoryService;
     @Autowired
