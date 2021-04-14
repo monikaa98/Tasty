cabc42c1b0562c5dd792c464e81cb5a2de73a610
monikaa98 <82385604+monikaa98@users.noreply.github.com>
2021-04-14 12:53:38 +0300

    Update RestaurantRepository.java

--- src/main/java/project/tastyfood/repository/RestaurantRepository.java
+++ src/main/java/project/tastyfood/repository/RestaurantRepository.java
@@ -1,10 +1,8 @@
 package project.tastyfood.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.tastyfood.model.entity.Restaurant;
-
 
 import java.util.List;
 import java.util.Optional;
